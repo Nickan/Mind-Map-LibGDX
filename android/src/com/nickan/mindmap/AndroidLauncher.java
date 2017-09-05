0779a3d380e543309eb8b4c59c18ef557cb6f8a8
Nickan <colete_nico@yahoo.com>
2017-09-05 05:21:01 -0700

    First Commit, saving all files for now.

---
+++ android/src/com/nickan/mindmap/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nickan.mindmap;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nickan.mindmap.MindMap;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MindMap(), config);
+	}
+}
