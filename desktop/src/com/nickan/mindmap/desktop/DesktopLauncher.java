362eb0decdee1fc23e840efd91c49faf35de9936
Nickan <Nickan@localhost>
2017-09-05 05:21:01 -0700

    First Commit, saving all files for now.

---
+++ desktop/src/com/nickan/mindmap/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nickan.mindmap.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nickan.mindmap.MindMap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MindMap(), config);
+	}
+}
